44528ec8f51ea2c8c44c4b09bc035505a61a808a
Naresh Parmar <parmarnaresh108@gmail.com>
2021-01-09 16:22:14 +0530

    Upload code to git

---
+++ lib/config_paytm.php
@@ -0,0 +1,5 @@
+<?php
+//Change the value of PAYTM_MERCHANT_KEY constant with details received from Paytm.
+//define("PAYTM_MERCHANT_KEY", "zFFSeeFXI2#B59tR"); 
+define("PAYTM_MERCHANT_KEY", "3l&tzCzmSSQM7vN2"); 
+?>
