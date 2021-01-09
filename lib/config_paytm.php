18b9902a3bdacf2cc36bdbeea369e0c033710baa
Naresh Parmar <parmarnaresh108@gmail.com>
2021-01-09 17:00:40 +0530

    Update config_paytm.php

--- lib/config_paytm.php
+++ lib/config_paytm.php
@@ -1,5 +1,5 @@
 <?php
 //Change the value of PAYTM_MERCHANT_KEY constant with details received from Paytm.
-//define("PAYTM_MERCHANT_KEY", "zFFSeeFXI2#B59tR"); 
-define("PAYTM_MERCHANT_KEY", "3l&tzCzmSSQM7vN2"); 
+//define("PAYTM_MERCHANT_KEY", "xxxxxxxx"); 
+define("PAYTM_MERCHANT_KEY", "xxxxxxx"); 
 ?>
